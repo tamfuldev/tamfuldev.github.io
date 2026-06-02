f3ba0cb7f7862b5c4386ef0e1079bbe5fac83004
TRANNGOCTAM <tran.ngoc.tam@uruca.jp>
2026-06-02 14:18:58 +0700

    update

--- src/components/LanguageButton.js
+++ src/components/LanguageButton.js
@@ -1,18 +1,23 @@
 import React from "react";
 import { useLanguage } from "./LanguageContext";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 const LanguageButton = () => {
     const { language, toggleLanguage } = useLanguage();
 
     return (
-        <button
-            type="button"
-            onClick={toggleLanguage}
-            className="control-btn"
-            aria-label="Toggle language"
-        >
-            {language.toUpperCase()}
-        </button>
+        <HelmetProvider>
+            <Helmet htmlAttributes={{ lang: language }} />
+
+            <button
+                type="button"
+                onClick={toggleLanguage}
+                className="control-btn"
+                aria-label="Toggle language"
+            >
+                {language.toUpperCase()}
+            </button>
+        </HelmetProvider>
     );
 };
 
